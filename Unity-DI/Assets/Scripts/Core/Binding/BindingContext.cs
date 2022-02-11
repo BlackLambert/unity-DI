4c81e46924a10710741fd56c087dc156ae5d7d5d
Sebastian Baier <sebastian.baier93@hotmail.de>
2022-02-12 00:17:23 +0100

    Binding context refactor

--- Unity-DI/Assets/Scripts/Core/Binding/BindingContext.cs
+++ Unity-DI/Assets/Scripts/Core/Binding/BindingContext.cs
@@ -3,8 +3,67 @@ using UnityEngine;
 
 namespace SBaier.DI
 {
-    public abstract class BindingContext
+
+    public class BindingContext<C1> : BindingContext<C1, C1, C1, C1, C1, C1, C1, C1>
+    {
+        public BindingContext(Binding binding, BindingStorage toContainerBinder) : base(binding, toContainerBinder) { }
+
+        public BindingContext<C1, TContract2> And<TContract2>(IComparable iD = default)
+		{
+            _toContainerBinder.Store<TContract2>(_binding, iD);
+            return new BindingContext<C1, TContract2>(_binding, _toContainerBinder);
+        }
+    }
+
+    public class BindingContext<C1, C2> : BindingContext<C1, C2, C2, C2, C2, C2, C2, C2>
+    {
+        public BindingContext(Binding binding, BindingStorage toContainerBinder) : base(binding, toContainerBinder) { }
+
+        public BindingContext<C1, C2, TContract3> And<TContract3>(IComparable iD = default)
+        {
+            _toContainerBinder.Store<TContract3>(_binding, iD);
+            return new BindingContext<C1, C2, TContract3>(_binding, _toContainerBinder);
+        }
+    }
+
+    public class BindingContext<C1, C2, C3> : BindingContext<C1, C2, C3, C3, C3, C3, C3, C3>
+    {
+        public BindingContext(Binding binding, BindingStorage toContainerBinder) : base(binding, toContainerBinder) { }
+
+        public BindingContext<C1, C2, C3, TContract4> And<TContract4>(IComparable iD = default)
+        {
+            _toContainerBinder.Store<TContract4>(_binding, iD);
+            return new BindingContext<C1, C2, C3, TContract4>(_binding, _toContainerBinder);
+        }
+    }
+
+    public class BindingContext<C1, C2, C3, C4> : BindingContext<C1, C2, C3, C4, C4, C4, C4, C4>
+    {
+        public BindingContext(Binding binding, BindingStorage toContainerBinder) : base(binding, toContainerBinder) { }
+    }
+
+    public class BindingContext<C1, C2, C3, C4, C5, C6, C7, C8> : BindingContext
 	{
+        public BindingContext(Binding binding, BindingStorage toContainerBinder) : base(binding, toContainerBinder) { }
+
+        public ToBindingContext<TConcrete> To<TConcrete>() where TConcrete : C1, C2, C3, C4, C5, C6, C7, C8
+        {
+            return new ToBindingContext<TConcrete>(_binding);
+        }
+
+        public FromNewBindingContext<TConcrete> ToNew<TConcrete>() where TConcrete : C1, C2, C3, C4, C5, C6, C7, C8, new()
+        {
+            return new FromNewBindingContext<TConcrete>(_binding);
+        }
+
+        public ToComponentBindingContext<TConcrete> ToComponent<TConcrete>() where TConcrete : Component, C1, C2, C3, C4, C5, C6, C7, C8
+        {
+            return new ToComponentBindingContext<TConcrete>(_binding);
+        }
+    }
+
+    public abstract class BindingContext
+    {
         protected readonly Binding _binding;
         protected readonly BindingStorage _toContainerBinder;
 
@@ -13,104 +72,6 @@ namespace SBaier.DI
         {
             _binding = binding;
             _toContainerBinder = toContainerBinder;
-        }
-    }
-
-    public class BindingContext<TContract> : BindingContext
-    {
-        public BindingContext(Binding binding, BindingStorage toContainerBinder) : base(binding, toContainerBinder) { }
-
-		public ToBindingContext<TConcrete> To<TConcrete>() where TConcrete : TContract
-        {
-            return new ToBindingContext<TConcrete>(_binding);
-        }
-
-        public FromNewBindingContext<TConcrete> ToNew<TConcrete>() where TConcrete : TContract, new()
-        {
-            return new FromNewBindingContext<TConcrete>(_binding);
-        }
-
-        public ToComponentBindingContext<TConcrete> ToComponent<TConcrete>() where TConcrete : Component, TContract
-        {
-            return new ToComponentBindingContext<TConcrete>(_binding);
-        }
-
-        public BindingContext<TContract, TContract2> And<TContract2>(IComparable iD = default)
-		{
-            _toContainerBinder.Store<TContract2>(_binding, iD);
-            return new BindingContext<TContract, TContract2>(_binding, _toContainerBinder);
-        }
-    }
-
-    public class BindingContext<TContract1, TContract2> : BindingContext
-	{
-        public BindingContext(Binding binding, BindingStorage toContainerBinder) : base(binding, toContainerBinder) { }
-
-        public ToBindingContext<TConcrete> To<TConcrete>() where TConcrete : TContract1, TContract2
-        {
-            return new ToBindingContext<TConcrete>(_binding);
-        }
-
-        public FromNewBindingContext<TConcrete> ToNew<TConcrete>() where TConcrete : TContract1, TContract2, new()
-        {
-            return new FromNewBindingContext<TConcrete>(_binding);
-        }
-
-        public ToComponentBindingContext<TConcrete> ToComponent<TConcrete>() where TConcrete : Component, TContract1, TContract2
-        {
-            return new ToComponentBindingContext<TConcrete>(_binding);
-        }
-
-        public BindingContext<TContract1, TContract2, TContract3> And<TContract3>(IComparable iD = default)
-        {
-            _toContainerBinder.Store<TContract3>(_binding, iD);
-            return new BindingContext<TContract1, TContract2, TContract3>(_binding, _toContainerBinder);
-        }
-    }
-
-    public class BindingContext<TContract1, TContract2, TContract3> : BindingContext
-	{
-        public BindingContext(Binding binding, BindingStorage toContainerBinder) : base(binding, toContainerBinder) { }
-
-        public ToBindingContext<TConcrete> To<TConcrete>() where TConcrete : TContract1, TContract2, TContract3
-        {
-            return new ToBindingContext<TConcrete>(_binding);
-        }
-
-        public FromNewBindingContext<TConcrete> ToNew<TConcrete>() where TConcrete : TContract1, TContract2, TContract3, new()
-        {
-            return new FromNewBindingContext<TConcrete>(_binding);
-        }
-
-        public ToComponentBindingContext<TConcrete> ToComponent<TConcrete>() where TConcrete : Component, TContract1, TContract2, TContract3
-        {
-            return new ToComponentBindingContext<TConcrete>(_binding);
-        }
-
-        public BindingContext<TContract1, TContract2, TContract3, TContract4> And<TContract4>(IComparable iD = default)
-        {
-            _toContainerBinder.Store<TContract3>(_binding, iD);
-            return new BindingContext<TContract1, TContract2, TContract3, TContract4>(_binding, _toContainerBinder);
-        }
-    }
-
-    public class BindingContext<TContract1, TContract2, TContract3, TContract4> : BindingContext
-	{
-        public BindingContext(Binding binding, BindingStorage toContainerBinder) : base(binding, toContainerBinder) { }
-
-        public ToBindingContext<TConcrete> To<TConcrete>() where TConcrete : TContract1, TContract2, TContract3, TContract4
-        {
-            return new ToBindingContext<TConcrete>(_binding);
-        }
-
-        public FromNewBindingContext<TConcrete> ToNew<TConcrete>() where TConcrete : TContract1, TContract2, TContract3, TContract4, new()
-        {
-            return new FromNewBindingContext<TConcrete>(_binding);
-        }
-
-        public ToComponentBindingContext<TConcrete> ToComponent<TConcrete>() where TConcrete : Component, TContract1, TContract2, TContract3, TContract4
-        {
-            return new ToComponentBindingContext<TConcrete>(_binding);
         }
     }
 }
