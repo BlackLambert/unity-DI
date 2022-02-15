aec644a9c8ccee297440ce2e94457134cabf1a61
Sebastian Baier <sebastian.baier93@hotmail.de>
2022-02-15 21:45:30 +0100

    Small adjustments

--- Unity-DI/Assets/Examples/CircularDependencyInjection/Scripts/CircularDependencyInstaller2.cs
+++ Unity-DI/Assets/Examples/CircularDependencyInjection/Scripts/CircularDependencyInstaller2.cs
@@ -4,10 +4,14 @@ namespace SBaier.DI.Examples.CircularDependencyInjection
 {
 	public class CircularDependencyInstaller2 : MonoInstaller
 	{
+		[SerializeField]
+		private Baz _bazPrefab;
+
 		public override void InstallBindings(Binder binder)
 		{
 			binder.BindToNewSelf<Foo>().AsSingle().NonLazy();
 			binder.BindToNewSelf<Bar>().AsSingle().NonLazy();
+			binder.BindComponent<Baz>().FromNewPrefabInstance(_bazPrefab).AsSingle().NonLazy();
 		}
 	}
 }
